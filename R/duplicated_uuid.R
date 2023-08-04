1cf7be700511501365e47329fc4b009bee2ad83b
Ahmed Mohamoud <axmedmaxamuud@yahoo.com>
2023-08-04 22:01:16 +0300

    duplicated uuids

---
+++ R/duplicated_uuid.R
@@ -0,0 +1,23 @@
+#' Check duplicated uuids
+#'
+#' @param data data in XML values format
+#'
+#' @return
+#' @export
+#'
+#' @examples
+check_duplicate_uuid <- function(data) {
+  if ("uuid" %in% names(data)) {
+    duplicate_uuid <- data[duplicated(data$uuid), "uuid"]
+
+    if (length(duplicate_uuid) > 0) {
+      message("The following UUID(s) are duplicated:")
+      message(paste(duplicate_uuid, collapse = ", "))
+    } else {
+      message("No duplicate UUID found.")
+    }
+  } else {
+    message("UUID variable not found in the dataset.")
+  }
+}
+
