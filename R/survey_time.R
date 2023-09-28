5482f18ecbb3e5a11c06e071c12ea735cea6ffc8
Ahmed Mohamoud <axmedmaxamuud@yahoo.com>
2023-09-29 02:03:30 +0300

    updating survey time function

--- R/survey_time.R
+++ R/survey_time.R
@@ -9,13 +9,21 @@
 #' @export
 #'
 #' @examples
-survey_time <- function(df, time_min, time_max){
-  df <- df %>% mutate(interview_duration = difftime(as.POSIXct(ymd_hms(end)), as.POSIXct(ymd_hms(start)), units = "mins"),
-                      CHECK_interview_duration = case_when(
-                        interview_duration < time_min ~ "Too short",
-                        interview_duration > time_max ~ "Too long",
-                        TRUE ~ "Okay"
-                      )
-  )
+
+survey_time <- function(df, time_min, time_max) {
+  # Check if 'start' and 'end' columns exist in the data frame
+  if (!all(c("start", "end") %in% colnames(df))) {
+    stop("The 'start' and 'end' columns are not found in the data.")
+  }
+
+  df <- df %>%
+    mutate(interview_duration = difftime(as.POSIXct(ymd_hms(end)), as.POSIXct(ymd_hms(start)), units = "mins"),
+           CHECK_interview_duration = case_when(
+             interview_duration < time_min ~ "Too short",
+             interview_duration > time_max ~ "Too long",
+             TRUE ~ "Okay"
+           )) %>%
+    filter(CHECK_interview_duration %in% c("Too short", "Too long"))
+
   return(df)
 }
